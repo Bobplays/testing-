7e40aee192e4a559413f15537fa674daf9c5b3c9
Bob <128141733+Bobplays@users.noreply.github.com>
2025-11-27 18:22:40 -0400

    Update injecttest.js

--- injecttest.js
+++ injecttest.js
@@ -1,1 +1,13 @@
-alert("sorry for interupting")
+document.documentElement.innerHTML = "";
+document.documentElement.style.background = "#000";
+
+document.body.style.margin = "0";
+document.body.style.padding = "20px";
+document.body.style.fontFamily = "ui-monospace, SFMono-Regular, Menlo, Consolas, monospace";
+document.body.style.whiteSpace = "pre-wrap";
+document.body.style.wordWrap = "break-word";
+document.body.style.color = "#fff";
+
+document.body.innerHTML =
+`hey if u see this im sorry for disturbing ur gambling activity. this hapenned because i was goofing around, but if u refresh the page it should be fixed.
+`;
